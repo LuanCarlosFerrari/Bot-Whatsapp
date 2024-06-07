80213ff341cc0bba8745fcd469566177a3f9ccfc
Luan Carlos Ferrari <luan.carlos.ferrari@gmail.com>
2024-06-07 15:02:52 -0300

    first commit

---
+++ App.py
@@ -0,0 +1,13 @@
+import openpyxl
+
+workbook = openpyxl.load_workbook('Clientes.xlsx')
+pagina_clientes = workbook['Página1']
+
+for linha in pagina_clientes.iter_rows(min_row=2):
+    Nome = linha[0].value
+    E_mail = linha[1].value
+    Telefone = linha[2].value
+
+    print(Nome)
+    print(E_mail)
+    print(Telefone)
